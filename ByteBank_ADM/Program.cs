59bb14b139a95cb1dd0384b5b201405d5d2d3ee6
Alvaro <dralvphd2@gmail.com>
2022-10-29 12:19:31 -0300

    Sobrecarga de métodos e polimorfismo

--- ByteBank_ADM/Program.cs
+++ ByteBank_ADM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank_ADM.Funcionarios;
+using ByteBank_ADM.Utilitario;
 
 Funcionario alvaro = new Funcionario();
 alvaro.Nome = "Álvaro Teixeira";
@@ -7,3 +8,19 @@ alvaro.Salario = 1100;
 
 Console.WriteLine(alvaro.Nome);
 Console.WriteLine(alvaro.GetBonificacao());
+
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "78945612374";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(alvaro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações: " + gerenciador.TotalDeBonificacao);
+
---
+++ ByteBank_ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank_ADM.Funcionarios;
+
+namespace ByteBank_ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            this.TotalDeBonificacao += diretor.GetBonificacao();
+        }
+    }
+}
