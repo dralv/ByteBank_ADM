0bb07c681d38899f88bf9b74407d8c1d0beb6948
Alvaro <dralvphd2@gmail.com>
2022-10-29 11:47:51 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank_ADM/Program.cs
@@ -0,0 +1,9 @@
+﻿using ByteBank_ADM.Funcionarios;
+
+Funcionario alvaro = new Funcionario();
+alvaro.Nome = "Álvaro Teixeira";
+alvaro.Cpf = "78945612300";
+alvaro.Salario = 1100;
+
+Console.WriteLine(alvaro.Nome);
+Console.WriteLine(alvaro.GetBonificacao());
